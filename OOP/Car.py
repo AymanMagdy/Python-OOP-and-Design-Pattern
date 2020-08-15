147ec4858fe728f68839756e16cec1972e3a0386
AymanMagdy <magdia883@gmail.com>
2020-08-15 22:28:41 +0200

    Finalize the file

--- OOP/Car.py
+++ OOP/Car.py
@@ -66,6 +66,3 @@ class EstateCars(Cars):
     def __init__(self, carYear, carModelName, carMotorType, carNumberOfDoors):
         carType = "Estate Cars"
         super().__init__(carType, carYear, carModelName, carMotorType, carNumberOfDoors)
-
-car = Pickups(2015, "ip", "d", "2")
-print(car.getCar())
